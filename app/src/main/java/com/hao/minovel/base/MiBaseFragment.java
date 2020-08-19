d34217d0dbc9ef3d574b00578d1429cd1b720592
haozhang <274595569@qq.com>
2020-08-19 18:23:53 +0800

    添加测滑的阴影效果

--- app/src/main/java/com/hao/minovel/base/MiBaseFragment.java
+++ app/src/main/java/com/hao/minovel/base/MiBaseFragment.java
@@ -34,7 +34,8 @@ public abstract class MiBaseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        roundLinearLayout = (RoundLinearLayout) inflater.inflate(R.layout.fragment_guide, container, false);
+        View v = inflater.inflate(R.layout.fragment_guide, container, false);
+        roundLinearLayout = v.findViewById(R.id.round_view);
         View content = inflater.inflate(onlayout(), container, false);
         roundLinearLayout.addView(content);
         roundLinearLayout.setCornerRadius((int) (maxRound * offset));
